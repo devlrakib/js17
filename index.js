3f8be542ca40a7496215efc4ba59393ebe1d0c32
devlrakib <devrakib5@gmail.com>
2024-01-13 03:18:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// For Loop Tutorial
+// For Loop Use Kora Hoi Sadharonoto Ak E kaj Barbar Korar Jonno
+// For Loop A Sadharonoto 3 Ta Jinish Use Kora Hoi (Starting, Condition, UptoDate)
+// for (starting; Condition; Update)   Example
+for (var x =1; x <=10; x=x+1)
+{
+    document.write("<h1>Bangladesh</h1>")
+}
+document.write("<h4>Text End</h4>")
+
+for( var x=1; x<=5; x=x+1){
+    document.write("<h1> Hello Javascript</h1>")
+}
+
+for(var x=1; x<=100; x=x+2){
+    document.write( " "+x)
+}
+
+for (var x=2; x<=100; x=x+2){
+    document.write(" "+x)
+}
